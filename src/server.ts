1043000b72e723f730afba4a1676a16f450f3607
Murtaza01 <merdada2000@hotmail.com>
2024-06-23 19:37:38 +0300

    added a way to upload files with multer, planning to save the files as a buffer in the db, then retrive it and send it to the front end as a biffer

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,21 @@ import express from "express";
 import bodyParser from "body-parser";
 import User from "./models/user";
 import { ConnectToDB } from "./database";
+import multer from "multer";
+import { writeFileSync } from "fs";
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage }).single("image");
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.post("/upload", upload, (req, res) => {
+  // console.log(req.file?.mimetype);
+  const buffer = req.file?.buffer as Buffer;
+  // writeFileSync("./image.jpg", buffer);
+});
 
 app.post("/new", (req, res) => {
   const userObj = req.body;
